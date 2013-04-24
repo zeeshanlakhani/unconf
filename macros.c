9e25019011930cea7bc29da0aa9a4f8faf568191
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2013-04-24 15:05:37 -0400

    make spaces not tabs

--- macros.c
+++ macros.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 #define IS_EVEN(_x) \
-	(_x & 1 ? "odd" : "even")
+    (_x & 1 ? "odd" : "even")
 
 int main(int argc, char *argv[])
 {
-	int i;
+    int i;
 
     if (argc < 2) {
-		printf("Not enough arguments");
-		return 0;
+        printf("Not enough arguments");
+        return 0;
 	}
 
-	for (i = 1; i < argc; i++) {
-		/* Convert to integer */
-		int val = atoi(argv[i]);
+    for (i = 1; i < argc; i++) {
+        /* Convert to integer */
+        int val = atoi(argv[i]);
 
-		printf( "The number %d is %s\n", val, IS_EVEN(val));
-	}
-	return 0;
+        printf( "The number %d is %s\n", val, IS_EVEN(val));
+    }
+    return 0;
 }
