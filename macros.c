36e9f42fba9d32fa4f19ca20fb26fab652c8d97b
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2013-04-24 15:06:18 -0400

    err

--- macros.c
+++ macros.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     if (argc < 2) {
         printf("Not enough arguments");
         return 0;
-	}
+    }
 
     for (i = 1; i < argc; i++) {
         /* Convert to integer */
